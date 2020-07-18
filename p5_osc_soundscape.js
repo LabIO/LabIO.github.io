ddb9c247cc3519605bb179acaa220542af6f5e2b
Laboratório de Interação e Oralidade <mengucci@gmail.com>
2020-07-18 21:54:44 +0100

    Update p5_osc_soundscape.js
    added different color for each wave

--- p5_osc_soundscape.js
+++ p5_osc_soundscape.js
@@ -1,12 +1,14 @@
-// hack this to leave a point playing and slowly disappear any time you click in the
+// hack this to leave a point playing and slowly disappear any time you click in the 
 // canvas. To create like a star soundscape
 
-let osc, playing, freq, amp;
+let osc, playing, freq, amp, stroke_color;
 
 function setup() {
-  let cnv = createCanvas(500,500);
+  let cnv = createCanvas(1000,800);
   cnv.mousePressed(playOscillator);
-
+  //cnv.style.position = 'static';
+  //cnv.style.z-index(-1);
+  // how to set the canvas in the background??
   osc = new p5.Oscillator('sine');
 
   choose_wave = createSelect();
@@ -14,22 +16,24 @@ function setup() {
   choose_wave.option('triangle');
   choose_wave.option('sawtooth');
   choose_wave.option('square');
-
+  
   choose_wave.changed( function() {
+    
     osc.setType(choose_wave.value() );
+    
   } );
 }
 
 function draw() {
-  // background(220)
+   //background(10, 30, 150); // how to set the background in the background? the strokes are not visible!
   freq = constrain(map(mouseX, 0, width, 100, 500), 100, 500);
   amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
 
   //text('freq: ' + freq, 20, 40);
   //text('amp: ' + amp, 20, 60);
+  
 
-
-
+  
   if (playing) {
     // smooth the transitions by 0.1 seconds
     osc.freq(freq, 0.1);
@@ -41,16 +45,28 @@ function playOscillator() {
   // starting an oscillator on a user gesture will enable audio
   // in browsers that have a strict autoplay policy.
   // See also: userStartAudio();
-  // SHOULD IT DRAW A POINT, NO?
+  // SHOULD IT DRAW A POINT, NO? 
   osc.start();
   playing = true;
-  stroke('purple'); // Change the color
+  //stroke('yellow'); // Change the color
+  if (choose_wave.value() == 'sine') {
+        stroke('yellow');
+        } 
+    else if (choose_wave.value() == 'triangle') {
+        stroke('pink');
+        }
+    else if (choose_wave.value() == 'sawtooth') {
+        stroke('red');
+        }
+    else if (choose_wave.value() == 'square') {
+        stroke('orange');
+        } // Change the color
   strokeWeight(10);
   point(mouseX,mouseY);
 }
 
 function mouseReleased() {
-  // ramp amplitude to 0 over 0.5 seconds
-  osc.amp(0, 0.5);
+  // ramp amplitude to 0 over 2 seconds
+  osc.amp(0, 2);
   playing = false;
 }
